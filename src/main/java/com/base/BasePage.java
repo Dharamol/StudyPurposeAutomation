34a383c732d6bfc5ccaa3256cc8af81dd8c7f342
Sandeep Somasundaran <sandeepsomasundaran@hotmail.com>
2026-03-19 15:30:32 +0530

    Basepage2

--- src/main/java/com/base/BasePage.java
+++ src/main/java/com/base/BasePage.java
@@ -18,7 +18,7 @@ public class BasePage{
 	public BasePage(WebDriver driver)
 	{
 	this.driver=driver;	
-	this.wait = new WebDriverWait(driver,Duration.ofSeconds(40));
+	this.wait = new WebDriverWait(driver,Duration.ofSeconds(20));
 	this.js=(JavascriptExecutor) driver;
 	}
 	
