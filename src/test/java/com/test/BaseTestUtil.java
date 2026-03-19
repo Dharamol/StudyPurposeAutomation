97e0661adbf62ecad256932775f4af237fc5ffc1
Sandeep Somasundaran <sandeepsomasundaran@hotmail.com>
2026-03-19 11:11:48 +0530

    baseTest

--- src/test/java/com/test/BaseTestUtil.java
+++ src/test/java/com/test/BaseTestUtil.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import com.utils.ConfigReader;
 
@@ -14,15 +15,28 @@ public class BaseTestUtil {
 	 protected WebDriver driver;
 	 
 	 String url =ConfigReader.get("saucedemo.url");
+	 
 	@BeforeMethod
 	public void driverManager()
 	{
 	
-		WebDriverManager.chromedriver().setup();
-		driver= new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get(url);
+//		WebDriverManager.chromedriver().setup();
+//		driver= new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get(url);
+//		
 		
+		WebDriverManager.chromedriver().setup();
+
+	    ChromeOptions options = new ChromeOptions();
+	    options.addArguments("--headless");        // IMPORTANT
+	    options.addArguments("--no-sandbox");      // REQUIRED in Jenkins
+	    options.addArguments("--disable-dev-shm-usage");
+
+	    driver = new ChromeDriver(options);
+
+	    driver.manage().window().maximize();
+	    driver.get(url);
 	}
 	
 	@AfterMethod
